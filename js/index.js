6db05f4ce05cb0f2e53bbd60ff3cdb0ae0b0ff2e
Julian03-lab <julian03yt@gmail.com>
2022-07-14 00:35:19 -0300

    Edit options added, about section updated

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function showAboutEditables() {
+    document.getElementById("editable-name").style.display ="inline"
+    document.getElementById("editable-title").style.display ="inline"
+    document.getElementById("editable-description").style.display ="inline"
+}
+
+function showEditOption(element){
+    document.getElementById(element).style.display = "inline"
+}
